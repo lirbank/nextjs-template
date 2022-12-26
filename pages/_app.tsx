cd7a64fc6cfcc29f74cf8812e375110947e26a55
Mikael Lirbank <mikael@lirbank.com>
2022-12-26 15:21:31 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,8 +13,13 @@ const inter = Inter({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${inter.variable} h-full font-sans`}>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
-    </main>
+    </>
   );
 }
