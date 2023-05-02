0a4edf376743042cc1d27332cfefc07e998ed1e3
Mikael Lirbank <mikael@lirbank.com>
2023-05-02 11:40:54 -0700

    Sync tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
-/**
- * https://nextjs.org/docs/basic-features/font-optimization#with-tailwind-css
- * See also pages/_app.tsx
- */
-const { fontFamily } = require("tailwindcss/defaultTheme");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
