47027d7f09bd19c13af57b7421f53dd0fc37531b
Mikael Lirbank <mikael@lirbank.com>
2023-06-19 15:48:11 -0700

    Move to app router

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main className="flex h-full">
+      <div className="m-auto text-center">
+        <div>nextjs-template</div>
+        <div>
+          <a
+            className="underline hover:no-underline"
+            href="https://github.com/lirbank/nextjs-template"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://github.com/lirbank/nextjs-template
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
